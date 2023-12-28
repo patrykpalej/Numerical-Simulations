86f51c9df65e28d896668ce901934e5b431cc3f4
Patryk Palej <patrykpalej@gmail.com>
2023-12-28 20:19:15 +0100

    Change titles

--- Project_Description.py
+++ Project_Description.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     st.markdown("""This project is a set of numerical simulations regarding
              various phenomena such as:""")
 
-    with st.expander("Gravity"):
+    with st.expander("Gravitational interactions"):
         st.markdown("""In this simulation, you can set the initial conditions
          for a group of material points, referred to as "planets." You have the option to input
          these conditions manually or import them from a file. Once set, you can customize
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         st.markdown("""Note that both saving logs and saving initial conditions to file is
          possible only when running locally.""")
 
-    with st.expander("Humans vs. Zombies"):
+    with st.expander("Humans vs. Zombies battle"):
         st.markdown("""In this simulation, two groups of characters fight each other to survive.
          Each character is defined by a set of characteristics, including their coordinates (x, y),
          velocity, and power. When a zombie wins a fight, it transforms a human into
